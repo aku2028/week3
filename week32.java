a701e47da0faf7729bf815cf74ae611c312413d6
aku2028 <akkuaccu1617@gmail.com>
2026-03-11 15:41:40 +0530

    practise1

---
+++ week32.java
@@ -0,0 +1,41 @@
+class week32 {
+    public static void main(String[] args) {
+
+        int[] num = new int[5];
+
+        // Taking input
+        for(int i = 0; i < num.length; i++) {
+            System.out.print("Enter number " + (i+1) + ": ");
+            num[i] = Integer.parseInt(System.console().readLine());
+        }
+
+        // Checking numbers
+        for(int i = 0; i < num.length; i++) {
+
+            if(num[i] > 0) {
+                if(num[i] % 2 == 0) {
+                    System.out.println(num[i] + " is positive and even");
+                } else {
+                    System.out.println(num[i] + " is positive and odd");
+                }
+            }
+            else if(num[i] < 0) {
+                System.out.println(num[i] + " is negative");
+            }
+            else {
+                System.out.println(num[i] + " is zero");
+            }
+        }
+
+        // Compare first and last element
+        if(num[0] == num[4]) {
+            System.out.println("First and last elements are equal");
+        }
+        else if(num[0] > num[4]) {
+            System.out.println("First element is greater than last element");
+        }
+        else {
+            System.out.println("First element is less than last element");
+        }
+    }
+}
